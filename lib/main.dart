707121c1e15e57505f031f63a397b3a5efaf4f65
kazukichi <20309063+n-kazukichi@users.noreply.github.com>
2023-06-23 12:53:49 +0900

    hooksつこてみる。

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // エントリポイント
 void main() {
@@ -19,62 +21,98 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // 初期画面にMyHomePageを指定。
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHooksPage('Flutter Demo Home Page With Hooks'),
     );
   }
 }
 
-// MyHomePageはStatefulWidget(なんらかのStateを持っているWidget)で...
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHooksPage extends HookConsumerWidget {
+  const MyHooksPage(this.title, {super.key});
   final String title;
 
   @override
-  // そのStateとWidgetの実態は_MyHomePageStateです。
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-// _MyHomePageStateは、↑のMyHomePageと紐づくStateを持っていて...
-class _MyHomePageState extends State<MyHomePage> {
-  // Stateはこの_counterですよ。
-  int _counter = 0;
-
-  // _counterというStateを書き換えるメソッド。
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  // そのStateにまつわるWidgetはここで組み立てます。
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // useStateでcounterを管理していますよ。
+    final counter = useState(0);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'counter',
             ),
             Text(
-              '$_counter',
+              '${counter.value}',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
-      // 画面右下のボタンを押すと_incrementCounterが呼ばれStateが書き換わる。
-      // すると、build()が呼ばれて画面が再描画される。
-      // その時 _counter の値が変わっているので、それが反映される。
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        // (+)ボタンを押したらstateの値を+1してますよ。
+        onPressed: () => counter.value++,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
   }
 }
+
+// // MyHomePageはStatefulWidget(なんらかのStateを持っているWidget)で...
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//   final String title;
+
+//   @override
+//   // そのStateとWidgetの実態は_MyHomePageStateです。
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// // _MyHomePageStateは、↑のMyHomePageと紐づくStateを持っていて...
+// class _MyHomePageState extends State<MyHomePage> {
+//   // Stateはこの_counterですよ。
+//   int _counter = 0;
+
+//   // _counterというStateを書き換えるメソッド。
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   // そのStateにまつわるWidgetはここで組み立てます。
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//           ],
+//         ),
+//       ),
+//       // 画面右下のボタンを押すと_incrementCounterが呼ばれStateが書き換わる。
+//       // すると、build()が呼ばれて画面が再描画される。
+//       // その時 _counter の値が変わっているので、それが反映される。
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
