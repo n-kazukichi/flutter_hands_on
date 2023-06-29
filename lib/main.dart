e7e399254e13217e0b681f95cebd5d74500ade6f
kazukichi <20309063+n-kazukichi@users.noreply.github.com>
2023-06-29 11:03:07 +0900

    カウントアップの独自hookを実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hands_on/hooks/use_counter.dart';
 import 'package:flutter_hands_on/pages/second_page.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -27,14 +28,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
+ValueNotifier<T> useLoggedState<T>(T initialData) {
+  final result = useState<T>(initialData);
+
+  useValueChanged<T, void>(result.value, (_, __) {
+    print(result.value);
+  });
+  return result;
+}
+
 class MyHooksPage extends HookConsumerWidget {
   const MyHooksPage(this.title, {super.key});
   final String title;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // useStateでcounterを管理していますよ。
-    final counter = useState(0);
+    // final counter = useLoggedState(0);
+    final counter = useCounter(0);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -62,65 +73,10 @@ class MyHooksPage extends HookConsumerWidget {
       ),
       floatingActionButton: FloatingActionButton(
         // (+)ボタンを押したらstateの値を+1してますよ。
-        onPressed: () => counter.value++,
+        onPressed: () => counter.increment(),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
   }
 }
-
-// // MyHomePageはStatefulWidget(なんらかのStateを持っているWidget)で...
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-//   final String title;
-
-//   @override
-//   // そのStateとWidgetの実態は_MyHomePageStateです。
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// // _MyHomePageStateは、↑のMyHomePageと紐づくStateを持っていて...
-// class _MyHomePageState extends State<MyHomePage> {
-//   // Stateはこの_counterですよ。
-//   int _counter = 0;
-
-//   // _counterというStateを書き換えるメソッド。
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   // そのStateにまつわるWidgetはここで組み立てます。
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       // 画面右下のボタンを押すと_incrementCounterが呼ばれStateが書き換わる。
-//       // すると、build()が呼ばれて画面が再描画される。
-//       // その時 _counter の値が変わっているので、それが反映される。
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
