6619cf2fa749fab0971ebef7944c6fc4eeccdd84
KazukiNaito <kato.kinako@gmail.com>
2023-09-07 13:32:08 +0900

    ユーザの新規登録や、既存ユーザの情報を引くなど。

--- lib/pages/firestore/firestore_profile_page.dart
+++ lib/pages/firestore/firestore_profile_page.dart
@@ -1,12 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hands_on/pages/firebase_auth/firebase_page.dart';
+import 'package:flutter_hands_on/pages/firestore/firestore_login_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-// ユーザのログイン状態を把握するプロバイダ。
-final userStreamProvider = StreamProvider<User?>((ref) {
-  return FirebaseAuth.instance.authStateChanges();
-});
 
 class FirestoreProfilePage extends HookConsumerWidget {
   const FirestoreProfilePage({super.key});
@@ -14,11 +9,12 @@ class FirestoreProfilePage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // ユーザのログイン状態を監視する。
-    final user = ref.watch(userStreamProvider);
+    final user = ref.watch(appUserProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Firestoreサンプル プロフ画面'),
+        automaticallyImplyLeading: false,
       ),
       body: Center(
         child: Column(
@@ -33,13 +29,29 @@ class FirestoreProfilePage extends HookConsumerWidget {
               if (user == null) {
                 // user が null すなわちログインしていないのでログインボタン表示。
                 return [
+                  const Text('未ログインです。ログインからやり直してください。'),
+                  const SizedBox(height: 50),
                   ElevatedButton(
-                    child: const Text("Googleログイン"),
-                    onPressed: () async => await signInWithGoogle(),
+                    child: const Text('戻る'),
+                    onPressed: () => Navigator.pop(context),
                   )
                 ];
               } else {
-                return [const CircularProgressIndicator()];
+                return [
+                  CircleAvatar(foregroundImage: NetworkImage(user['avater'])),
+                  Text(user['name']),
+                  Text('所在地:${user["locale"]}'),
+                  Text('自己紹介:${user["description"]}'),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                      child: const Text("ログアウト"),
+                      onPressed: () async {
+                        await signOut();
+                        if (context.mounted) {
+                          Navigator.pop(context);
+                        }
+                      })
+                ];
               }
             }, error: (e, t) {
               // ユーザの監視中に何らかのエラーが発生。
@@ -49,15 +61,5 @@ class FirestoreProfilePage extends HookConsumerWidget {
         ),
       ),
     );
-  }
-}
-
-/// ログインボタンだけを表示する部品。
-class LoginWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-        child: const Text("Googleログイン"),
-        onPressed: () async => await signInWithGoogle());
   }
 }
